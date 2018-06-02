142df89d3de6f63a2efec5498e9a3e10836cee82
Nate Denys <nate_denys@yahoo.com>
2018-06-02 10:49:04 -0700

    create cartModule
    using flux patterns and axios to get/post to server

--- src/app/store/modules/cart/index.js
+++ src/app/store/modules/cart/index.js
@@ -1,10 +1,56 @@
-const state = {};
+import axios from 'axios';
 
-const mutations = {};
+const state = {
+    cartItems: []
+};
 
-const actions = {};
+const mutations = {
+    UPDATE_CART_ITEMS (state, payload) {
+        state.cartItems = payload;
+    }
+};
 
-const getters = {};
+// create actions to collect, add, delete one, or delete all items
+const actions = {
+    getCartItems ({ commit }) {
+        axios.get('/api/cart').then((response) => {
+            commit('UPDATE_CART_ITEMS', response.data)
+        });
+    },
+    addCartItem ({ commit }, cartItem) {
+        axios.post('/api/cart', cartItem).then((response) => {
+            commit('UPDATE_CART_ITEMS', response.data)
+        });
+    },
+    deleteCartItem ({ commit }, cartItem) {
+        axios.post('/api/cart/delete', cartItem).then((response) => {
+            commit('UPDATE_CART_ITEMS', response.data)
+        });
+    },
+    removeAllCartItems ({ commit }) {
+        axios.post('/api/cart/delete/all').then((response) => {
+            commit('UPDATE_CART_ITEMS', response.data)
+        });
+    }
+};
+
+// get cartItems from Vuex
+const getters = {
+    cartItems: state => state.cartItems,
+    // create getter for retunring the cart total from products in cart
+    cartTotal: state => {
+        return state.cartItems.reduce((acc, cartItem) => {
+            return (cartItem.quantity * cartItem.price) + acc;
+
+        }, 0).toFixed(2);
+    },
+    // return total quantity of items in my cart
+    cartQuantity: state => {
+        return state.cartItems.reduce((acc, cartItem) => {
+            return cartItem.quantity + acc;
+        }, 0);
+    }
+};
 
 const cartModule = {
     state,
